54dec1c1378f7c7eaa7c998313e277cb28088c0c
Yi Xiaodong <409070169@qq.com>
2020-07-16 12:23:32 +0800

    api finish

--- db_init.py
+++ db_init.py
@@ -4,6 +4,7 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["runoobdb"]
 collection_names = ["userlist","fmex"]
 
+
 if __name__ == '__main__':
 
     for name in collection_names:
